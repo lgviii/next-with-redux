d06bcba6f40e2d35f9d2b670b10fa38db989d642
DESKTOP-CON99DG\lgvii <lgviii@gmail.com>
2020-06-07 01:17:55 -0700

    Added redux tools.

--- store.js
+++ store.js
@@ -1,13 +1,26 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 // import rootReducer from "./rootReducer/index";
 import rootReducer from "./pagesWithStores/admin/reducer";
 
-// export const makeStore = (context) => {
-//   return createStore(rootReducer, context, applyMiddleware(thunk));
-// };
+const composeType = () => {
+  console.log("redux tool running");
+  if (process.browser) {
+    return compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+  } else {
+    return compose(applyMiddleware(thunk));
+  }
+};
 
-export const makeStore = (context) => createStore(rootReducer);
+export const makeStore = () => {
+  return createStore(rootReducer, composeType());
+};
+
+// export const makeStore = (context) => createStore(rootReducer);
 
 export const wrapper = createWrapper(makeStore, { debug: true });
