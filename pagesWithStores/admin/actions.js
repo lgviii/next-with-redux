0836acd216fce2e7feb5213fbb90b1faae59972f
DESKTOP-CON99DG\lgvii <lgviii@gmail.com>
2020-06-09 23:40:16 -0700

    Trying to connect to BE

--- pagesWithStores/admin/actions.js
+++ pagesWithStores/admin/actions.js
@@ -13,13 +13,21 @@ export function loadData(inputText) {
   currentUser.getIdToken().then((token) => {
     console.log("token");
     console.log(token);
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
+
+    // fetch("https://jsonplaceholder.typicode.com/todos/1")
+    fetch("https://localhost:5001/weatherforecast")
+      .then((response) => {
+        console.log("api response");
+        console.log(response);
+        return response.json();
+      })
       .then((data) => {
-        console.log("Got api response");
+        console.log("api response data");
+        console.log(data);
         store.dispatch({
           type: LOAD,
-          payload: inputText + "***" + data.title,
+          // payload: inputText + "***" + data.title,
+          payload: inputText + "***" + data.summary,
         });
       });
   });
